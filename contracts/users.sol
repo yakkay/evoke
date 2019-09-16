d66ada437bd7d6fa6694b94b020a0546acbbb6f5
Yakkay0 <yakkay@gmail.com>
2019-09-16 18:51:55 -0500

    user mission

--- contracts/users.sol
+++ contracts/users.sol
@@ -4,6 +4,7 @@ contract Users{
     struct user {
         address account;
         uint[] missions;
+        bool active;
     }
     mapping (string => user) users;
     address Bob;
@@ -12,10 +13,18 @@ contract Users{
         Bob = msg.sender;
     }
 
-    function setUser(string memory mail,address _account,uint _mission)public{
+    function setUser(string memory mail,address _account)public returns(bool success){
         require(msg.sender == Bob, "You are not Bob");
         users[mail].account = _account;
+        users[mail].active = true;
+        return true;
+    }
+
+    function approveMission(string memory mail,uint _mission)public returns(bool success){
+        require(msg.sender == Bob, "You are not Bob");
+        require(users[mail].active,"User does not exist or is not active");
         users[mail].missions.push(_mission);
+        return true;
     }
 
     function getUser(string memory mail)public view returns(address _account,uint[] memory _mission){
