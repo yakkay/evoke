71433dba1a729e1e1c0e9b724698665fc074e195
Yakkay0 <yakkay@gmail.com>
2019-12-19 16:13:22 -0500

    lilicif update

--- v6/test/app_test.js
+++ v6/test/app_test.js
@@ -69,7 +69,7 @@ request.post('http://localhost:3001/evocoin/transfer', {
   json: { 
       'addressfrom': '0xE401862558e44fa2547b66a6C1D50c8492718997',
       'privatekey':'57a29559e91df761c933986caf25debac5e21f4056d4487150cdcaab5cd37096',
-      'addressto': '0x253D2A04a47d5919176ac994c153C60CaD379f81',
+      'addressto': '0xFfe660a6de7128e04A0E8F37086AB6d601C58D04',
       'amount':46
   }
 }, (error, res, body) => {
