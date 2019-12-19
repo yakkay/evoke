58ac9d2cb8047b2f331233e6f61745233c841b33
Yakkay0 <yakkay@gmail.com>
2019-12-19 15:45:28 -0500

    nrodriguez update

--- v6/test/app_test.js
+++ v6/test/app_test.js
@@ -69,7 +69,7 @@ request.post('http://localhost:3001/evocoin/transfer', {
   json: { 
       'addressfrom': '0xE401862558e44fa2547b66a6C1D50c8492718997',
       'privatekey':'57a29559e91df761c933986caf25debac5e21f4056d4487150cdcaab5cd37096',
-      'addressto': '0x4Fe82Da8CDcf9E4120ECc4841B4CBE019eD8e555',
+      'addressto': '0x44ca642f4503B3418f302AD775F562E792139c07',
       'amount':46
   }
 }, (error, res, body) => {
