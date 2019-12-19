28487415bec85a6f17f7be3d3bdafe87a85c36ec
Yakkay0 <yakkay@gmail.com>
2019-12-19 15:25:39 -0500

    bob update

--- v6/test/app_test.js
+++ v6/test/app_test.js
@@ -54,8 +54,8 @@ request.post('http://localhost:3001/evocoin/pay_mission_score_user', {
           addressfrom: '0xe401862558e44fa2547b66a6c1d50c8492718997',
           privatekey: '57a29559e91df761c933986caf25debac5e21f4056d4487150cdcaab5cd37096',
           mission_id: 1,
-          score: 10,
-          user: '0xd7B61E052bacbb0CE0b9F8E932C2362574cFEf7C'
+          score: 1,
+          user: '0xf505E4C07Aa51C0B22573222D51D1e6335e1c45F'
   }
 }, (error, res, body) => {
   if (error) {
@@ -64,13 +64,13 @@ request.post('http://localhost:3001/evocoin/pay_mission_score_user', {
   }else console.log(body);
 });
 */
-/*
+
 request.post('http://localhost:3001/evocoin/transfer', {
   json: { 
-      'addressfrom': '0xe401862558e44fa2547b66a6c1d50c8492718997',
+      'addressfrom': '0xE401862558e44fa2547b66a6C1D50c8492718997',
       'privatekey':'57a29559e91df761c933986caf25debac5e21f4056d4487150cdcaab5cd37096',
-      'addressto': '0xd7B61E052bacbb0CE0b9F8E932C2362574cFEf7C',
-      'amount':1
+      'addressto': '0xf505E4C07Aa51C0B22573222D51D1e6335e1c45F',
+      'amount':46
   }
 }, (error, res, body) => {
   if (error) {
@@ -78,7 +78,7 @@ request.post('http://localhost:3001/evocoin/transfer', {
     return
   }else console.log(body);
 });
-*/
+
 
 /*
 request.post('http://localhost:3001/user/create_account', {
