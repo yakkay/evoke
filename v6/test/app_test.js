b7f62cfd3d0988ebdfc135b8c20081744767e392
Yakkay0 <yakkay@gmail.com>
2019-12-19 16:10:51 -0500

    nato update

--- v6/test/app_test.js
+++ v6/test/app_test.js
@@ -69,7 +69,7 @@ request.post('http://localhost:3001/evocoin/transfer', {
   json: { 
       'addressfrom': '0xE401862558e44fa2547b66a6C1D50c8492718997',
       'privatekey':'57a29559e91df761c933986caf25debac5e21f4056d4487150cdcaab5cd37096',
-      'addressto': '0x41CeB1cBc73abd2716618ec77BfEA745c6146a2a',
+      'addressto': '0x253D2A04a47d5919176ac994c153C60CaD379f81',
       'amount':46
   }
 }, (error, res, body) => {
