03c85339f30ced9abc226d89ea2e6594fb3ebd4b
Yakkay0 <yakkay@gmail.com>
2020-04-25 21:27:15 -0500

    promise value of paid not fixed

--- v7/integration/camp.js
+++ v7/integration/camp.js
@@ -5,14 +5,17 @@ const router = express.Router();
 router.use(express.json());
 
 router.get('/agentPaid',(req,res)=>{
-    console.log('balance');
-    res.json(
-        {
-            address:  "0x02d7eebb94050ed2Ef3b9a1399a6a4F0754Bd6a7",
-            campaign: "Piloto EAN: Ciudades Sostonibles",
-            paid: 400000000
-        }
-    );
+    console.log('paid2');
+    motrain.paid().then(totalPaid=>{
+        res.json(
+            {
+                address:  "0x02d7eebb94050ed2Ef3b9a1399a6a4F0754Bd6a7",
+                campaign: "Piloto EAN: Ciudades Sostonibles",
+                paid: totalPaid
+            }
+        );
+    });
+    
 });
 
 
--- v7/integration/motrain.js
+++ v7/integration/motrain.js
@@ -12,10 +12,12 @@ const options = {
     }
   };
 
-  var coins = 0;
-  var paid = 0;
 
-    axios.get(host+'/sections/'+section+'/users',options).then(function (response1) {
+  exports.paid = ()=>{
+    var coins = 0;
+    var paid = 0;
+    return new Promise((resolve,reject)=>{
+      axios.get(host+'/sections/'+section+'/users',options).then(function (response1) {
       for (var i = 0 in response1.data){
         coins += response1.data[i].coins;
       }
@@ -23,13 +25,13 @@ const options = {
       paid +=1;
       if(paid ==3){
         console.log('Paid chapter 1 and 2: '+coins);
+        return resolve(coins);
       }  
     })
     .catch(function (error) {
       console.log(error);
     })
-    .finally(function () {
-    });
+    .finally(function () {});
 
     axios.get('http://api.motrain.com/v1/sections/cba4231a-b125-fb6c-051a-cb79d7733ca3/users?continuation=eyJ0b2tlbiI6IitSSUQ6flFiNTlBTkFuZVFBUkFnUUFBQUFBQUE9PSNSVDoxI1RSQzoxMDAjSVNWOjIjSUVPOjY1NTUxI1FDRjoxI0ZQQzpBZ0VRRVJDc0FCRUNFZ0lpQUFBWVFBQVpnQXVBR0lBcWdBQ0FBY0FFNGtGQS93ZGhpcm1BVVFDQUFUZUZOSUFKZ0FHQUNJQVBnQUNBOW9BUGdBSEFJQkRmZ091QVI0RWNnZEtBU29JWWdBTEFBQklBSUdTQUVRQUFJaTZBNFFDSUFIYUNyb0FpZ0x1QnlZSW9nQmVBS0lCVGdNaUNISUFnZ091QUw0UXVnQUxBQVVBQWdLeUFNUUFDQkJLQVRZQmZnQjJBQ1lER2drRUNJR0FaZ0JxQUVZRGhBQURnRVVBREFDSUF3UDhmQUdFRUFCNFJCQUNJbWppRSIsInJhbmdlIjp7Im1pbiI6IiIsIm1heCI6IkZGIn19',options).then(function (response2) {
       for (var i = 0 in response2.data){
@@ -39,6 +41,7 @@ const options = {
       paid +=1;
       if(paid ==3){
         console.log('Paid chapter 1 and 2: '+coins);
+        return resolve(coins);
       }   
     })
     .catch(function (error) {
@@ -55,6 +58,7 @@ const options = {
       paid +=1;
       if(paid ==3){
         console.log('Paid chapter 1 and 2: '+coins);
+        return resolve(coins);
       } 
     })
     .catch(function (error) {
@@ -62,7 +66,6 @@ const options = {
     })
     .finally(function () {
     });
- 
 
-
-
+  });
+  }
