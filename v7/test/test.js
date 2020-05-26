52d707a305231e1a870d0b6decca876043f1fe2a
Yakkay0 <yakkay@gmail.com>
2020-05-26 17:32:11 -0500

    test

--- v7/test/test.js
+++ v7/test/test.js
@@ -2,41 +2,39 @@ const axios = require('axios');
 const options = {
   headers: {
       accept: "application/json"
-  }
+  },
 };
+/*
+axios.get('http://localhost:3001/user/create_account').then(function(response){
+  console.log(response.data.address)
+})*/
 
+axios.post('http://172.18.0.16:3000/create-mootivated-bc-users/',
+{
+  "id_motrain": "30b723ef-fa97-5d3e-9777-9958c20d0c64",
+  "pv_key": "0x888a4a896170c8324219908a494638f1c7d0c0a7e9f9011aecb6687be53da163",
+  "address": "0x2C7c80b44c430befA6c46aE9BfED8813C118366E"
+}
+).then(function(response){
+  console.log(response.data);
+}).catch(function(error){
+  console.log(error);
+});
+/*
 axios.get('http://localhost:3001/campaign/',options).then(function(response){
   console.log(response.data);
 }).catch(function(error){
   console.log(error);
-});
+});*/
 /*
-axios.get('http://localhost:3001/user/id/2',options).then(function(response){
+axios.get('http://localhost:3001/user/id/1',options).then(function(response){
   console.log(response.data);
 }).catch(function(error){
   console.log(error);
 });
 */
-/*
-axios.get('http://localhost:3001/campaign/budget',options).then(function(response){
-  console.log(response.data);
-}).catch(function(error){
-  console.log(error);
-});
 
-axios.get('http://localhost:3001/campaign/redeemed',options).then(function(response){
-  console.log(response.data);
-}).catch(function(error){
-  console.log(error);
-});
-*/
-/*
-axios.get('http://localhost:3001/user/balance?id=2',options).then(function(response){
-  console.log(response.data);
-}).catch(function(error){
-  console.log(error);
-});
-*/
+
 /*const request = require('request');
 
 request.post('http://localhost:3001/evocoin/balanceOf', {
@@ -52,50 +50,6 @@ request.post('http://localhost:3001/evocoin/balanceOf', {
 
 request.get('http://localhost:3001/evocoin/totalSupply', {
 
-}, (error, res, body) => {
-  if (error) {
-    console.error(error)
-    return
-  }else console.log(body);
-});
-
-request.get('http://localhost:3001/evocoin/get_mission_score_reward', {
-  json: {
-          mission_id: 1,
-          score: 10
-  }
-}, (error, res, body) => {
-  if (error) {
-    console.error(error)
-    return
-  }else console.log(body);
-});
-
-
-request.post('http://localhost:3001/evocoin/set_mission_score_reward', {
-  json: {
-          addressfrom: '0xe401862558e44fa2547b66a6c1d50c8492718997',
-          privatekey: '57a29559e91df761c933986caf25debac5e21f4056d4487150cdcaab5cd37096',
-          mission_id: 1,
-          score: 10,
-          reward: 2
-  }
-}, (error, res, body) => {
-  if (error) {
-    console.error(error)
-    return
-  }else console.log(body);
-});
-
-
-request.post('http://localhost:3001/evocoin/pay_mission_score_user', {
-  json: {
-          addressfrom: '0xe401862558e44fa2547b66a6c1d50c8492718997',
-          privatekey: '57a29559e91df761c933986caf25debac5e21f4056d4487150cdcaab5cd37096',
-          mission_id: 1,
-          score: 1,
-          user: '0xf505E4C07Aa51C0B22573222D51D1e6335e1c45F'
-  }
 }, (error, res, body) => {
   if (error) {
     console.error(error)
