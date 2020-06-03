dae2c369ede08544d1fcc9316c1bf0752ca0277b
Yakkay0 <yakkay@gmail.com>
2020-06-03 03:15:30 -0500

    fix bug

--- v8/dashboard/main.js
+++ v8/dashboard/main.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const camp = require('./campaign')
-const app = express()
-app.use('/campaign', camp)
 const port = 3000
 const message = '<h1>Evoke Colombia 2020</h1><p> Dashboard enpoint: GET /campaign</p>'
 
+const app = express()
+app.use('/campaign', camp)
 app.get('/*',(req,res)=>{res.status(200).send(message);});
 
 app.listen(port,()=>{console.log(`Evoke dahsboard API server running ${port} port.`)});
