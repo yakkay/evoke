05e0832119b9b7b7698f9e53046eb4b79c20148b
Yakkay0 <yakkay@gmail.com>
2020-06-03 02:50:15 -0500

    permanent Payment and redemption to blockchain

--- v8/dashboard/main.js
+++ v8/dashboard/main.js
@@ -2,7 +2,7 @@ const express = require('express')
 const camp = require('./campaign')
 const app = express()
 app.use('/campaign', camp)
-const port = 3001
+const port = 3000
 const message = '<h1>Evoke Colombia 2020</h1><p> Dashboard enpoint: GET /campaign</p>'
 
 app.get('/*',(req,res)=>{res.status(200).send(message);});
